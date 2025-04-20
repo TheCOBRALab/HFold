8ead19c80f9f9e80e5697823fd05e8268b858e08
Yahya Al-Shamali <yaalshama@gmail.com>
2025-04-20 05:25:54 -0600

    More Test Cases

---
+++ test/tests/helper.hpp
@@ -0,0 +1,29 @@
+#ifndef HELPER_H
+#define HELPER_H
+#include <string>
+
+struct HFoldParams {
+    std::string sequence = "";
+    std::string restricted = "";
+    double energy = 0.0;
+    sparse_tree* tree = nullptr;
+    bool pk_free = false;
+    bool pk_only = false;
+    int dangles = 2;
+    int suboptCount = 1;
+    std::string fileI = "";
+    std::string fileO = "";
+    std::string paramFile = "rna_DirksPierce09.par";
+    bool noConv_given = false;
+};
+
+std::string capture_console_output(const std::function<void()>& func);
+std::string get_output(const std::string& file_path, bool delete_after_read = false);
+std::vector<std::vector<Result>> hfold_test(HFoldParams& p);
+
+// terminal output strings
+extern const std::string traceback_cases_output;
+extern const std::string input_file_and_sequence_output;
+extern const std::string input_and_output_file_output;
+
+#endif
