ae9f23fc04a3089f5457e0ca8cfecb425e6b744b
Yahya Al-Shamali <42281247+PointlessUser@users.noreply.github.com>
2025-05-23 15:59:03 -0600

    macos support

--- pylib/build_vienna.sh
+++ pylib/build_vienna.sh
@@ -8,9 +8,32 @@ else
   curl -L -O https://github.com/ViennaRNA/ViennaRNA/releases/download/v2.7.0/ViennaRNA-2.7.0.tar.gz
   tar -xzf ViennaRNA-2.7.0.tar.gz
   cd ViennaRNA-2.7.0
+
+  # Bug fixes in dlib
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+      # typedef with the standard UTF-32 code unit.
+      sed -i.bak 's/typedef uint32 unichar;/typedef char32_t unichar;/' \
+          src/dlib-19.24/dlib/unicode/unicode.h
+
+      # make the new type serialisable
+      sed -i.bak '/USE_DEFAULT_INT_SERIALIZATION_FOR(uint64)/a\
+      USE_DEFAULT_INT_SERIALIZATION_FOR(char32_t)
+      ' src/dlib-19.24/dlib/serialize.h
+
+      # “template” keyword.  Add the empty <> that upstream already has.
+      sed -i.bak 's/::template go(/::template go<>(/' \
+          src/dlib-19.24/dlib/global_optimization/find_max_global.h
+  fi
+
   ./configure --without-perl --without-python --disable-shared --enable-static --prefix=/tmp/vienna-cache
   make -j$(nproc)
-  make install
+
+  # override pkgpyexecdir so "make install" never touches /RNA (/RNA needs sudo access)
+  make  -j"${CPU_COUNT}" pkgpyexecdir="${PY_SITE_DIR}" install
+
+  cd ..
+  rm -rf ViennaRNA-2.7.0
+  rm -f ViennaRNA-2.7.0.tar.gz
 fi
 
 # Set paths for build
